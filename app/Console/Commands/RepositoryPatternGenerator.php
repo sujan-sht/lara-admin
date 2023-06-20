0791e2396843d22b4a487cad029530455732f215
sujan-sht <sujanstha016@gmail.com>
2023-06-20 14:46:36 +0545

    repo with request command

--- app/Console/Commands/RepositoryPatternGenerator.php
+++ app/Console/Commands/RepositoryPatternGenerator.php
@@ -12,22 +12,23 @@ class RepositoryPatternGenerator extends Command
      *
      * @var string
      */
-    protected $signature = 'make:repo {name}';
+    protected $signature = 'make:repo {name} {--r|request : Make a request file for repo pattern}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create a new repository pattern';
+    protected $description = 'Create a new repository pattern with request file if -r is used';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $name=$this->argument('name');
-        RepositoryPatternService::repoPattern($name);
+        $name = $this->argument('name');
+        $makeRequest = $this->option('request');
+        RepositoryPatternService::repoPattern($name,$makeRequest);
         $this->info('Repository Pattern created successfully for model: '. $name);
     }
 }
